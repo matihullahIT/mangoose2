d95487f7adc51e7c85ce1760469fb987fca8a1de
Matih Ur Rehman <matihurrehman.official@gmail.com>
2022-11-01 15:20:01 +0500

    saperation

---
+++ index.js
@@ -0,0 +1,4 @@
+const mongoose= require("mongoose")
+const conn= require('./Controler/dbconnect.js')
+const mood=require("./modle/modle")
+const crud=require("./modle/crud")
--- modle/modle.js
+++ modle/modle.js
@@ -1,5 +1,4 @@
 const mongoose=require("mongoose")
-const tbname='userdata'
 const userdata=mongoose.Schema({
     id:{
         type :Number,
@@ -12,16 +11,5 @@ const userdata=mongoose.Schema({
         type :String,
     }
 },{timestamp:true})
-const entrdta=async()=>{
-    const user= new mongoose.model('newdbs',tbname)
-    const Dta=new user({id:1212,name:'Wasif ali wasif',profile:'sufi'})
-    let result = await Dta.save();
-    console.log(result)
-}
-module.exports=entrdta();
-const find =async()=>{
-    const mdl= new mongoose.model('newdbs',tbname)
-    const User=  await mdl.findOne({id:1212});
-    console.log(User);
-}
-module.exports=find();
+    const user= new mongoose.model('newdbs',userdata)
+    module.exports=user;
