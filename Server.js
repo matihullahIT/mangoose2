9a84c4746d69870ee8641449e7e1824ccbfb0104
Matih Ur Rehman <matihurrehman.official@gmail.com>
2022-11-15 00:43:25 +0500

    apis in server js

--- Server.js
+++ Server.js
@@ -1,12 +1,12 @@
 const mongoose= require("mongoose")
-const conn= require('./Controler/dbconnect.js')
+const conn= require('./config/dbconnect.js')
 const mod=require("./modle/modle")
 const express= require("express")
 const app = express()
 const port= 3000;
 app.listen(port)
 app.use(express.json())
-console.log(`app is connected with port    ${port}`)
+console.log(`app is connected with port:    ${port}`)
 app.post("/",(req,resp)=>{
   console.log(req.body)
   let dta=new mod({
@@ -16,18 +16,24 @@ app.post("/",(req,resp)=>{
   })
   resp.send(dta)
   dta.save()
-  console.log("data hasbeen saved successfully")
+  console.log("data has been saved successfully")
 })
 app.delete("/",(req,resp)=>{
     mod.findOneAndDelete({id:req.body.id})
-    resp.send("Data \n"+mod+" \n has been deleted successfully")
+    resp.send("Data has been deleted successfully")
     })
     app.post('/update',(req,res)=>{
       mod.findOneAndUpdate({id:req.body.id},req.body)
           .then(()=>res.send("data is update"))
           .catch(err=>res.send(err));
       })
-app.get("/",(req,resp)=>{
-  resp.send(mod)
+app.get("/find",(req,resp)=>{
   mod.find()
+  .then(data=>resp.send(data))
+  .catch(err=>resp.send(err))
+})
+app.get("/find-id",(req,res)=>{
+  mod.find({id:req.body.id})
+  .then(data=>res.send(data))
+  .catch(err=>res.send(err));
 })
