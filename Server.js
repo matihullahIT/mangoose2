1e02dcc045b0757a44ad1a3758f132e4cdd1e369
Matih Ur Rehman <matihurrehman.official@gmail.com>
2022-11-17 13:37:05 +0500

    all done

--- Server.js
+++ Server.js
@@ -1,39 +1,21 @@
-const mongoose= require("mongoose")
-const conn= require('./config/dbconnect.js')
-const mod=require("./modle/modle")
-const express= require("express")
-const app = express()
-const port= 3000;
-app.listen(port)
+const express=require("express");
+const app =express();
 app.use(express.json())
-console.log(`app is connected with port:    ${port}`)
-app.post("/",(req,resp)=>{
-  console.log(req.body)
-  let dta=new mod({
-    id:req.body.id,
-    name:req.body.name,
-    profile:req.body.profile
-  })
-  resp.send(dta)
-  dta.save()
-  console.log("data has been saved successfully")
+const port=4000;
+const connection=require('./config/dbconnect')
+const userrouts=require("./Routes/routs")
+app.get("/",(req,resp)=>{
+  resp.send("the api is working properly")
 })
-app.delete("/",(req,resp)=>{
-    mod.findOneAndDelete({id:req.body.id})
-    resp.send("Data has been deleted successfully")
-    })
-    app.post('/update',(req,res)=>{
-      mod.findOneAndUpdate({id:req.body.id},req.body)
-          .then(()=>res.send("data is update"))
-          .catch(err=>res.send(err));
-      })
-app.get("/find",(req,resp)=>{
-  mod.find()
-  .then(data=>resp.send(data))
-  .catch(err=>resp.send(err))
-})
-app.get("/find-id",(req,res)=>{
-  mod.find({id:req.body.id})
-  .then(data=>res.send(data))
-  .catch(err=>res.send(err));
+const dbconn=async ()=>{
+  try{
+    await connection();
+    console.log("db is connected succecssfuly");
+  }
+  catch(error){
+    console.log(error)
+  }
+}
+app.listen(port,()=>{
+  console.log(`server is working properly on port number${port}`)
 })
